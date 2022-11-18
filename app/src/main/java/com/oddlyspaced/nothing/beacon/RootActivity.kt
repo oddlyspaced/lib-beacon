fdf7d27eefee47abebf7bc06986ee6bbfe072f58
oddlyspaced <oddlyspaced1@gmail.com>
2022-11-19 01:53:31 +0530

    app: RootActivity: test led pattern

--- app/src/main/java/com/oddlyspaced/nothing/beacon/RootActivity.kt
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/RootActivity.kt
@@ -2,10 +2,14 @@ package com.oddlyspaced.nothing.beacon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oddlyspaced.nothing.beacon.lib.LedAnimator
+import com.oddlyspaced.nothing.beacon.lib.RootLedControllerImpl
 
 class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
+
+        LedAnimator(RootLedControllerImpl()).testPattern()
     }
 }
