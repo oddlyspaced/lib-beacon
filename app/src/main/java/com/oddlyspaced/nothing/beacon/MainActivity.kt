1c5f389191c989a6c8b705f665cb8ba72fd3069f
oddlyspaced <oddlyspaced1@gmail.com>
2022-11-19 01:08:24 +0530

    app: setup dummy implementation for getting root acces

--- app/src/main/java/com/oddlyspaced/nothing/beacon/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.oddlyspaced.nothing.beacon
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.BuildConfig
+import com.topjohnwu.superuser.Shell
 
 class MainActivity : AppCompatActivity() {
+
+    init {
+        Shell.enableVerboseLogging = BuildConfig.DEBUG
+        Shell.setDefaultBuilder(
+            Shell.Builder.create().apply {
+                setFlags(Shell.FLAG_REDIRECT_STDERR)
+                setTimeout(10)
+            }
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Shell.getShell {
+            startActivity(Intent(this, RootActivity::class.java))
+        }
     }
 }
