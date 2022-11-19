44e8f825dcd487b0e42eb7f2c97997f10a596587
oddlyspaced <oddlyspaced1@gmail.com>
2022-11-20 04:22:59 +0530

    app: MainActivity: setup base ui [2]

--- app/src/main/java/com/oddlyspaced/nothing/beacon/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/MainActivity.kt
@@ -3,18 +3,18 @@ package com.oddlyspaced.nothing.beacon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Device
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,7 +49,9 @@ fun GlyphToggle() {
         mutableStateOf(true)
     }
     Card(
-        modifier = Modifier.fillMaxWidth().padding(top = 28.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 28.dp),
         backgroundColor = if (switchState.value) Main50 else Main10,
         shape = RoundedCornerShape(24.dp)
     ) {
@@ -75,12 +77,63 @@ fun GlyphToggle() {
     }
 }
 
+private fun translateBrightnessToAlpha(brightness: Int): Float {
+    (brightness / 4095f).let {
+        return if (it < 0.1f) 0.1f else it
+    }
+}
+
+@Composable
+fun PhoneLayout(cameraBrightness: Int, vlineBrightness: Int, centerBrightness: Int, bottomStripBrightness: Int, bottomDotBrightness: Int) {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.padding(top = 24.dp)) {
+        Image(
+            painter = painterResource(id = com.oddlyspaced.nothing.beacon.lib.R.drawable.back),
+            contentDescription = "",
+        )
+        Image(
+            painter = painterResource(id = com.oddlyspaced.nothing.beacon.lib.R.drawable.led_strip_camera),
+            contentDescription = "",
+            alpha = translateBrightnessToAlpha(cameraBrightness)
+        )
+        Image(
+            painter = painterResource(id = com.oddlyspaced.nothing.beacon.lib.R.drawable.led_strip_vline),
+            contentDescription = "",
+            alpha = translateBrightnessToAlpha(vlineBrightness)
+        )
+        Image(
+            painter = painterResource(id = com.oddlyspaced.nothing.beacon.lib.R.drawable.led_strip_center),
+            contentDescription = "",
+            alpha = translateBrightnessToAlpha(centerBrightness)
+        )
+        Image(
+            painter = painterResource(id = com.oddlyspaced.nothing.beacon.lib.R.drawable.led_strip_bottom),
+            contentDescription = "",
+            alpha = translateBrightnessToAlpha(bottomStripBrightness)
+        )
+        Image(
+            painter = painterResource(id = com.oddlyspaced.nothing.beacon.lib.R.drawable.led_dot_bottom),
+            contentDescription = "",
+            alpha = translateBrightnessToAlpha(bottomDotBrightness)
+        )
+    }
+}
+
 @Composable
 fun Content() {
-    Column() {
+    Column {
         Column(modifier = Modifier.padding(top = 80.dp, start = 16.dp, end = 16.dp)) {
             Heading()
             GlyphToggle()
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f),
+                    color = Color.Transparent,
+                ) {
+                    PhoneLayout(0, 0, 0, 0, 0)
+                }
+            }
+
         }
     }
 }
