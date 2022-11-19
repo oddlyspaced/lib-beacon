088de8f80265355c81922dc1e101a0c8a5e680a1
oddlyspaced <oddlyspaced1@gmail.com>
2022-11-20 02:49:36 +0530

    app: Color: define fixed material 3 colors

--- app/src/main/java/com/oddlyspaced/nothing/beacon/ui/theme/Color.kt
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/ui/theme/Color.kt
@@ -2,7 +2,16 @@ package com.oddlyspaced.nothing.beacon.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Main0 = Color(0xFFF7FDFF)
+val Main10 = Color(0xFFF8FDFF)
+val Main50 = Color(0xFFD1F7FF)
+val Main100 = Color(0xFFA5EEFE)
+val Main200 = Color(0xFF8CD0E0)
+val Main300 = Color(0xFF73B5C3)
+val Main400 = Color(0xFF5C99A7)
+val Main500 = Color(0xFF427E8A)
+val Main600 = Color(0xFF2C6571)
+val Main700 = Color(0xFF0F4D58)
+val Main800 = Color(0xFF00343D)
+val Main900 = Color(0xFF001F24)
+val Main1000 = Color(0xFF000000)
