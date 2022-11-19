d4e3de0906d8ad439bd59c9001f035f7fd8d4806
oddlyspaced <oddlyspaced1@gmail.com>
2022-11-19 17:03:40 +0530

    app: RootActivity: setup sample demo for led view

--- app/src/main/java/com/oddlyspaced/nothing/beacon/RootActivity.kt
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/RootActivity.kt
@@ -1,15 +1,38 @@
 package com.oddlyspaced.nothing.beacon
 
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oddlyspaced.nothing.beacon.databinding.ActivityRootBinding
 import com.oddlyspaced.nothing.beacon.lib.LedAnimator
 import com.oddlyspaced.nothing.beacon.lib.RootLedControllerImpl
+import kotlin.math.abs
 
 class RootActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityRootBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        setContentView(binding.root)
 
-        LedAnimator(RootLedControllerImpl()).testPattern()
+        binding.nothingDevice.apply {
+            setOnCameraSectionClickListener {
+                cameraBrightness = abs(cameraBrightness - 4095)
+            }
+            setOnVLineSectionClickListener {
+                slantBrightness = abs(slantBrightness - 4095)
+            }
+            setOnCenterSectionClickListener {
+                centerBrightness = abs(centerBrightness - 4095)
+            }
+            setOnBottomStripSectionClickListener {
+                bottomStripBrightness = abs(bottomStripBrightness - 4095)
+            }
+            setOnBottomDotSectionClickListener {
+                bottomDotBrightness = abs(bottomDotBrightness - 4095)
+            }
+        }
+
     }
 }
