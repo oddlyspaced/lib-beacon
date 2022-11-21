04a505bf6f3ed091ecc95677b9ca2d5e27c7b53c
oddlyspaced <oddlyspaced1@gmail.com>
2022-11-21 16:40:07 +0530

    lib: LedAnimator: add method for wired charging animation

--- lib/src/main/java/com/oddlyspaced/nothing/beacon/lib/LedAnimator.kt
+++ lib/src/main/java/com/oddlyspaced/nothing/beacon/lib/LedAnimator.kt
@@ -1,9 +1,14 @@
 package com.oddlyspaced.nothing.beacon.lib
 
 import android.animation.ValueAnimator
+import com.oddlyspaced.nothing.beacon.lib.constant.Led
 import com.oddlyspaced.nothing.beacon.lib.constant.LedConstant
 import com.oddlyspaced.nothing.beacon.lib.constant.Section
 import com.oddlyspaced.nothing.beacon.lib.interfaces.LedController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LedAnimator(private val controller: LedController) {
     fun testPattern() {
@@ -17,4 +22,30 @@ class LedAnimator(private val controller: LedController) {
             repeatCount = 1
         }.start()
     }
+
+    fun wiredChargingAnimation(batteryValue: Int) {
+        val leds = arrayOf(16, 13, 11, 9, 12, 10, 14, 15, 8)
+        controller.setSectionBrightness(Section.ALL_WHITE_LEDS, 0)
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(1000)
+            // fill all leds to max
+            for (led in leds) {
+                controller.setLedBrightness(Led.fromCode(led), 4095)
+                delay(16)
+            }
+            // wait for some secs
+            delay(16)
+            // calculate how much leds to fill for battery percentage
+            // uptoLed = index corresponding to leds array which tells as to what all leds should remain lighted
+            val uptoLed = ((batteryValue / 100.0) * leds.size).toInt()
+            // turn off all leds after uptoLed index
+            for (i in (uptoLed until leds.size).reversed()) {
+                if (i == 0) // let dot led remain lit
+                    continue
+                controller.setLedBrightness(Led.fromCode(leds[i]), 0)
+                delay(16)
+            }
+        }
+    }
+
 }
