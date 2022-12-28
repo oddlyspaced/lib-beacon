ae7316584e853fcbb784deaeb88a9d36ebd36e1b
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-28 23:57:26 +0530

    app: save working phone call receiver state

---
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/receiver/PhoneCallReceiver.java
@@ -0,0 +1,81 @@
+package com.oddlyspaced.nothing.beacon.receiver;
+
+import java.util.Date;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+
+// https://gist.github.com/ftvs/e61ccb039f511eb288ee <3
+public abstract class PhoneCallReceiver extends BroadcastReceiver {
+
+    //The receiver will be recreated whenever android feels like it.  We need a static variable to remember data between instantiations
+
+    private static int lastState = TelephonyManager.CALL_STATE_IDLE;
+    private static Date callStartTime;
+    private static boolean isIncoming;
+    private static String savedNumber;  //because the passed incoming is only valid in ringing
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //We listen to two intents.  The new outgoing call only tells us of an outgoing call.  We use it to get the number.
+        if (intent.getAction().equals("android.intent.action.NEW_OUTGOING_CALL")) {
+            savedNumber = intent.getExtras().getString("android.intent.extra.PHONE_NUMBER");
+        }
+        else{
+            TelephonyManager telephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            telephony.listen(new PhoneStateListener() {
+                @Override
+                public void onCallStateChanged(int state, String phoneNumber) {
+                    onCustomCallStateChanged(context, state, phoneNumber);
+                }
+            }, PhoneStateListener.LISTEN_CALL_STATE);
+        }
+    }
+
+    //Derived classes should override these to respond to specific events of interest
+    protected void onIncomingCallStarted(Context ctx, String number, Date start){}
+    protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end){}
+    protected void onMissedCall(Context ctx, String number, Date start){}
+
+    //Deals with actual events
+
+    //Incoming call-  goes from IDLE to RINGING when it rings, to OFFHOOK when it's answered, to IDLE when its hung up
+    //Outgoing call-  goes from IDLE to OFFHOOK when it dials out, to IDLE when hung up
+    public void onCustomCallStateChanged(Context context, int state, String number) {
+        if(lastState == state){
+            //No change, debounce extras
+            return;
+        }
+        switch (state) {
+            case TelephonyManager.CALL_STATE_RINGING:
+                isIncoming = true;
+                callStartTime = new Date();
+                savedNumber = number;
+                onIncomingCallStarted(context, number, callStartTime);
+                break;
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+                //Transition of ringing->offhook are pickups of incoming calls.  Nothing done on them
+                if(lastState != TelephonyManager.CALL_STATE_RINGING){
+                    isIncoming = false;
+                    callStartTime = new Date();
+                }
+                break;
+            case TelephonyManager.CALL_STATE_IDLE:
+                //Went to idle-  this is the end of a call.  What type depends on previous state(s)
+                if(lastState == TelephonyManager.CALL_STATE_RINGING){
+                    //Ring but no pickup-  a miss
+                    onMissedCall(context, savedNumber, callStartTime);
+                }
+                else if(isIncoming){
+                    onIncomingCallEnded(context, savedNumber, callStartTime, new Date());
+                }
+                break;
+        }
+        lastState = state;
+    }
+}
--- app/src/main/java/com/oddlyspaced/nothing/beacon/receiver/PhoneStateReceiver.kt
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/receiver/PhoneStateReceiver.kt
@@ -1,24 +1,57 @@
 package com.oddlyspaced.nothing.beacon.receiver
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.util.Log
-import com.oddlyspaced.nothing.beacon.lib.LedAnimator
-import com.oddlyspaced.nothing.beacon.lib.RootLedControllerImpl
+import com.oddlyspaced.nothing.beacon.util.Logger
+import java.util.*
 
-class PhoneStateReceiver: BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d("SPACEWAR", "Receiver Receving ${context == null}")
-//        if (context != null) {
-//            Log.d("SPACEWAR", "Context Null!")
-//        }
-        LedAnimator(RootLedControllerImpl()).resourceAnimation(context!!, com.oddlyspaced.nothing.beacon.lib.R.raw.data_abra, com.oddlyspaced.nothing.beacon.lib.R.raw.ringtone_abra)
-//        intent?.let { it ->
-//            Log.d("SPACEWAR", it.action.toString() + " // " + it.extras!!.keySet().toTypedArray())
-//            it.extras!!.keySet().forEachIndexed { index, s ->
-//                Log.d("SPACEWAR_KEY", s)
+//
+//class PhoneStateReceiver: BroadcastReceiver() {
+//
+//    private var lastState = "123"
+//
+//    // RINGING -> OFFHOOK -> IDLE
+//    override fun onReceive(_context: Context?, _intent: Intent?) {
+//        _context?.let { context ->
+//            _intent?.let { intent ->
+//                if (intent.action != null && intent.action ==  TelephonyManager.ACTION_PHONE_STATE_CHANGED) {
+//                    Logger.d("PhoneStateReceiver: action: $lastState  " + intent.extras?.getString(TelephonyManager.EXTRA_STATE))
+//                    lastState = intent.extras?.getString(TelephonyManager.EXTRA_STATE).toString()
+//                }
 //            }
 //        }
+////        LedAnimator(RootLedControllerImpl()).resourceAnimation(context!!, com.oddlyspaced.nothing.beacon.lib.R.raw.data_abra, com.oddlyspaced.nothing.beacon.lib.R.raw.ringtone_abra)
+//    }
+//}
+
+class PhoneStateReceiver: PhoneCallReceiver() {
+    override fun onCustomCallStateChanged(context: Context?, state: Int, number: String?) {
+        Logger.d("onCustomCallStateChanges: Phone Number: $number $state")
+        super.onCustomCallStateChanged(context, state, number)
     }
+
+    override fun onIncomingCallEnded(ctx: Context?, number: String?, start: Date?, end: Date?) {
+        Logger.d("onIncomingCallEnded")
+        super.onIncomingCallEnded(ctx, number, start, end)
+    }
+
+    override fun onIncomingCallStarted(ctx: Context?, number: String?, start: Date?) {
+        Logger.d("onIncomingStarted")
+        super.onIncomingCallStarted(ctx, number, start)
+    }
+
+    override fun onMissedCall(ctx: Context?, number: String?, start: Date?) {
+        Logger.d("onMissedCall")
+        super.onMissedCall(ctx, number, start)
+    }
+
+    override fun onOutgoingCallEnded(ctx: Context?, number: String?, start: Date?, end: Date?) {
+        Logger.d("onOutgoingCallEnded")
+        super.onOutgoingCallEnded(ctx, number, start, end)
+    }
+
+    override fun onOutgoingCallStarted(ctx: Context?, number: String?, start: Date?) {
+        Logger.d("onOutgoingCallStarted")
+        super.onOutgoingCallStarted(ctx, number, start)
+    }
+
 }
