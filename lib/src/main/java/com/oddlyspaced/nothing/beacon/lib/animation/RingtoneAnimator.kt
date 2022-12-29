0d92d97e19a633347bde6a75f330b27fb774cedb
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-29 09:04:32 +0530

    app: RingtoneAnimation: optimize light sync with sound

--- lib/src/main/java/com/oddlyspaced/nothing/beacon/lib/animation/RingtoneAnimator.kt
+++ lib/src/main/java/com/oddlyspaced/nothing/beacon/lib/animation/RingtoneAnimator.kt
@@ -9,16 +9,17 @@ import com.oddlyspaced.nothing.beacon.lib.constant.Led
 import com.oddlyspaced.nothing.beacon.lib.constant.Section
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.logging.Logger
 
-class RingtoneAnimator(private val context: Context) {
+class RingtoneAnimator(context: Context) {
 
     private val controller = RootLedControllerImpl()
     private val mediaPlayer = MediaPlayer.create(context, R.raw.ringtone_radiate)
     private val ledAnimationData = arrayListOf<Array<String>>()
 
     init {
-
         context.resources.openRawResource(R.raw.data_radiate).bufferedReader().readLines()
             .forEach {
                 ledAnimationData.add(it.split(",").toTypedArray())
@@ -37,20 +38,17 @@ class RingtoneAnimator(private val context: Context) {
     private fun animate() {
         CoroutineScope(Dispatchers.IO).launch {
             while (isRinging) {
-                if (mediaPlayer.currentPosition < mediaPlayer.duration - 1) {
-//                    Log.d("OKK", "${mediaPlayer.currentPosition} ${mediaPlayer.duration}")
-                    // spread data across duration
+                if (mediaPlayer.currentPosition < mediaPlayer.duration) {
                     val durLoc = mediaPlayer.currentPosition.toDouble() / mediaPlayer.duration
                     val pos = (durLoc * ledAnimationData.size).toInt()
-                    val data = ledAnimationData[pos]
-                    controller.setLedBrightness(Led.fromCode(7), data[0].toInt())
-                    controller.setLedBrightness(Led.fromCode(1), data[1].toInt())
-                    controller.setSectionBrightness(Section.CENTER_RING, data[2].toInt())
-                    controller.setSectionBrightness(Section.SLANT, data[3].toInt())
-                    controller.setLedBrightness(Led.fromCode(16), data[4].toInt())
-                }
-                else {
-                    animate()
+                    if (pos < ledAnimationData.size) {
+                        val data = ledAnimationData[pos]
+                        controller.setLedBrightness(Led.fromCode(7), data[0].toInt())
+                        controller.setLedBrightness(Led.fromCode(1), data[1].toInt())
+                        controller.setSectionBrightness(Section.CENTER_RING, data[2].toInt())
+                        controller.setSectionBrightness(Section.SLANT, data[3].toInt())
+                        controller.setLedBrightness(Led.fromCode(16), data[4].toInt())
+                    }
                 }
             }
         }
