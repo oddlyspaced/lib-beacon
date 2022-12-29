0ca0e42c323d9788f4c3853b9db56df3e595bcee
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-29 08:18:17 +0530

    app: MainActivity, TestActivity: setup sample to gain root access

--- app/src/main/java/com/oddlyspaced/nothing/beacon/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.oddlyspaced.nothing.beacon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oddlyspaced.nothing.beacon.databinding.ActivityMainBinding
+import com.topjohnwu.superuser.Shell
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    init {
+        Shell.enableVerboseLogging = BuildConfig.DEBUG
+        Shell.setDefaultBuilder(
+            Shell.Builder.create().apply {
+                setFlags(Shell.FLAG_REDIRECT_STDERR)
+                setTimeout(10)
+            }
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        Shell.getShell {
+            startActivity(Intent(this, TestActivity::class.java))
+            finish()
+        }
     }
 }
