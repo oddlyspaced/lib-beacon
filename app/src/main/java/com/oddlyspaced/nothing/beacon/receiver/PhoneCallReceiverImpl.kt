f30c9b8915737ae593534caa6565bf2b45c97b0b
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-29 09:03:47 +0530

    app: PhoneCallReceiverImpl: make use of led service constants

--- app/src/main/java/com/oddlyspaced/nothing/beacon/receiver/PhoneCallReceiverImpl.kt
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/receiver/PhoneCallReceiverImpl.kt
@@ -2,13 +2,13 @@ package com.oddlyspaced.nothing.beacon.receiver
 
 import android.content.Context
 import android.content.Intent
+import com.oddlyspaced.nothing.beacon.service.LedHandlerService
 import com.oddlyspaced.nothing.beacon.util.Logger
 import java.util.*
 
 class PhoneCallReceiverImpl: PhoneCallReceiver() {
     override fun onCustomCallStateChanged(context: Context?, state: Int, number: String?) {
         Logger.d("onCustomCallStateChanges: Phone Number: $number $state")
-        context?.sendBroadcast(Intent("com.oddly.led"))
         super.onCustomCallStateChanged(context, state, number)
     }
 
@@ -19,18 +19,18 @@ class PhoneCallReceiverImpl: PhoneCallReceiver() {
 
     override fun onIncomingCallStarted(ctx: Context?, number: String?, start: Date?) {
         Logger.d("onIncomingStarted")
-        sendBroadcast(ctx!!, "started")
+        sendBroadcast(ctx!!, LedHandlerService.ACTION_CALL_STARTED)
         super.onIncomingCallStarted(ctx, number, start)
     }
 
     override fun onMissedCall(ctx: Context?, number: String?, start: Date?) {
         Logger.d("onMissedCall")
-        sendBroadcast(ctx!!, "missed")
+        sendBroadcast(ctx!!, LedHandlerService.ACTION_CALL_MISSED)
         super.onMissedCall(ctx, number, start)
     }
 
     private fun sendBroadcast(context: Context, event: String) {
-        context.sendBroadcast(Intent("com.oddly.led.$event"))
+        context.sendBroadcast(Intent(event))
     }
 
 }
