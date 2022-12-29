decae5ca1adc34075fbffea6ee4078baf39bb8d1
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-29 09:02:51 +0530

    app: TestActivity: add buttons to ask perms and start service

--- app/src/main/java/com/oddlyspaced/nothing/beacon/TestActivity.kt
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/TestActivity.kt
@@ -2,10 +2,34 @@ package com.oddlyspaced.nothing.beacon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.oddlyspaced.nothing.beacon.databinding.ActivityTestBinding
+import com.oddlyspaced.nothing.beacon.service.LedHandlerService
 
 class TestActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityTestBinding.inflate(layoutInflater) }
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean -> }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        setContentView(binding.root)
+
+        binding.btnPermCallLog.setOnClickListener {
+            requestPermissionLauncher.launch(android.Manifest.permission.READ_CALL_LOG)
+        }
+
+        binding.btnPermNotification.setOnClickListener {
+            requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
+
+        binding.btnPermPhone.setOnClickListener {
+            requestPermissionLauncher.launch(android.Manifest.permission.READ_PHONE_STATE)
+        }
+
+        binding.btnStartService.setOnClickListener {
+            LedHandlerService.start(applicationContext)
+        }
     }
 }
