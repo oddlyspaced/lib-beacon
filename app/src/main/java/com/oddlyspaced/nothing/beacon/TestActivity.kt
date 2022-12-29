b1d021690bfaa0f2767783278ddee7693fd0c6a3
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-29 09:38:41 +0530

    app: TestActivity: add testing code for animation and ringtones

--- app/src/main/java/com/oddlyspaced/nothing/beacon/TestActivity.kt
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/TestActivity.kt
@@ -2,19 +2,33 @@ package com.oddlyspaced.nothing.beacon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.oddlyspaced.nothing.beacon.databinding.ActivityTestBinding
+import com.oddlyspaced.nothing.beacon.lib.animation.RingtoneAnimator
+import com.oddlyspaced.nothing.beacon.lib.enum.NothingRingtone
 import com.oddlyspaced.nothing.beacon.service.LedHandlerService
 
 class TestActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityTestBinding.inflate(layoutInflater) }
+    private val ringtoneAnimations = NothingRingtone.values()
 
-    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean -> }
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        if (!isGranted) {
+            Toast.makeText(applicationContext, "Please grant that permission!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private lateinit var ringtoneAnimator: RingtoneAnimator
+    private var currentRingtone = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        ringtoneAnimator = RingtoneAnimator(applicationContext, NothingRingtone.ABRA)
+        binding.txCurrentAnim.text = NothingRingtone.ABRA.name
 
         binding.btnPermCallLog.setOnClickListener {
             requestPermissionLauncher.launch(android.Manifest.permission.READ_CALL_LOG)
@@ -30,6 +44,38 @@ class TestActivity : AppCompatActivity() {
 
         binding.btnStartService.setOnClickListener {
             LedHandlerService.start(applicationContext)
+        }
+
+        binding.btnRingStart.setOnClickListener {
+            if (!ringtoneAnimator.isRinging) {
+                ringtoneAnimator.play()
+            }
+        }
+
+        binding.btnRingStop.setOnClickListener {
+            if (ringtoneAnimator.isRinging) {
+                ringtoneAnimator.stop()
+            }
+        }
+
+        binding.btnAnimNext.setOnClickListener {
+            ringtoneAnimator.stop()
+            currentRingtone++
+            if (currentRingtone == ringtoneAnimations.size) {
+                currentRingtone = 0
+            }
+            binding.txCurrentAnim.text = ringtoneAnimations[currentRingtone].name
+            ringtoneAnimator = RingtoneAnimator(applicationContext, ringtoneAnimations[currentRingtone])
+        }
+
+        binding.btnAnimPrev.setOnClickListener {
+            ringtoneAnimator.stop()
+            currentRingtone--
+            if (currentRingtone == -1) {
+                currentRingtone = ringtoneAnimations.size - 1
+            }
+            binding.txCurrentAnim.text = ringtoneAnimations[currentRingtone].name
+            ringtoneAnimator = RingtoneAnimator(applicationContext, ringtoneAnimations[currentRingtone])
         }
     }
 }
