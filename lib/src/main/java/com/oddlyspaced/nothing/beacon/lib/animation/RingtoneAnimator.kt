08ba12351bee276215f9a3d59cb074939504cae2
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-29 07:56:34 +0530

    lib: RingtoneAnimator: Move ringtone animation logic to class

---
+++ lib/src/main/java/com/oddlyspaced/nothing/beacon/lib/animation/RingtoneAnimator.kt
@@ -0,0 +1,66 @@
+package com.oddlyspaced.nothing.beacon.lib.animation
+
+import android.content.Context
+import android.media.MediaPlayer
+import android.util.Log
+import com.oddlyspaced.nothing.beacon.lib.R
+import com.oddlyspaced.nothing.beacon.lib.RootLedControllerImpl
+import com.oddlyspaced.nothing.beacon.lib.constant.Led
+import com.oddlyspaced.nothing.beacon.lib.constant.Section
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class RingtoneAnimator(private val context: Context) {
+
+    private val controller = RootLedControllerImpl()
+    private val mediaPlayer = MediaPlayer.create(context, R.raw.ringtone_radiate)
+    private val ledAnimationData = arrayListOf<Array<String>>()
+
+    init {
+
+        context.resources.openRawResource(R.raw.data_radiate).bufferedReader().readLines()
+            .forEach {
+                ledAnimationData.add(it.split(",").toTypedArray())
+            }
+    }
+
+    private var isRinging = false
+
+    fun play() {
+        mediaPlayer.start()
+        mediaPlayer.isLooping = true
+        isRinging = true
+        animate()
+    }
+
+    private fun animate() {
+        CoroutineScope(Dispatchers.IO).launch {
+            while (isRinging) {
+                if (mediaPlayer.currentPosition < mediaPlayer.duration - 1) {
+//                    Log.d("OKK", "${mediaPlayer.currentPosition} ${mediaPlayer.duration}")
+                    // spread data across duration
+                    val durLoc = mediaPlayer.currentPosition.toDouble() / mediaPlayer.duration
+                    val pos = (durLoc * ledAnimationData.size).toInt()
+                    val data = ledAnimationData[pos]
+                    controller.setLedBrightness(Led.fromCode(7), data[0].toInt())
+                    controller.setLedBrightness(Led.fromCode(1), data[1].toInt())
+                    controller.setSectionBrightness(Section.CENTER_RING, data[2].toInt())
+                    controller.setSectionBrightness(Section.SLANT, data[3].toInt())
+                    controller.setLedBrightness(Led.fromCode(16), data[4].toInt())
+                }
+                else {
+                    animate()
+                }
+            }
+        }
+    }
+
+    fun stop() {
+        isRinging = false
+        mediaPlayer.seekTo(0)
+        mediaPlayer.stop()
+        mediaPlayer.prepareAsync()
+    }
+
+}
