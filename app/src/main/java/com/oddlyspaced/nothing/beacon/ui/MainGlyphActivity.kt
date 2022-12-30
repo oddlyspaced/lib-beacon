3161c73f93d83a5f7e81987be454b2cb10025421
oddlyspaced <oddlyspaced1@gmail.com>
2022-12-30 21:47:50 +0530

    app: MainGlyphActivity: init

---
+++ app/src/main/java/com/oddlyspaced/nothing/beacon/ui/MainGlyphActivity.kt
@@ -0,0 +1,12 @@
+package com.oddlyspaced.nothing.beacon.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.nothing.beacon.R
+
+class MainGlyphActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_glyph)
+    }
+}
