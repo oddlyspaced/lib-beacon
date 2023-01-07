31195eeff1b2efaf2769a565e3d583f2e73dfab1
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-07 18:03:41 +0530

    lib: NothingLEDView: allow custom bottom intensity and darken device with high led brightness

--- lib/src/main/java/com/oddlyspaced/nothing/beacon/lib/ui/NothingLEDView.kt
+++ lib/src/main/java/com/oddlyspaced/nothing/beacon/lib/ui/NothingLEDView.kt
@@ -4,11 +4,16 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.oddlyspaced.nothing.beacon.lib.R
 import com.oddlyspaced.nothing.beacon.lib.databinding.DeviceBinding
+import com.oddlyspaced.nothing.beacon.lib.enum.LedConstant
 
 class NothingLEDView: ConstraintLayout {
 
     private lateinit var binding: DeviceBinding
+
+    var darkenCameraSection = true
 
     constructor(context: Context): super(context) {
         init(context)
@@ -24,42 +29,73 @@ class NothingLEDView: ConstraintLayout {
 
     private fun init(context: Context) {
         binding = DeviceBinding.inflate(LayoutInflater.from(context), this, true)
+        allLedBrightness = 0
     }
 
     private fun translateBrightnessToAlpha(brightness: Int): Float {
-        (brightness / 4095f).let {
+        (brightness / LedConstant.BRIGHTNESS_MAX.toFloat()).let {
             return if (it < 0.1f) 0.1f else it
         }
     }
 
-    var cameraBrightness = 4095
+    var cameraBrightness = LedConstant.BRIGHTNESS_MAX
         set(value) {
             field = value
             binding.imgDeviceLedCamera.alpha = translateBrightnessToAlpha(value)
         }
 
-    var slantBrightness = 4095
+    var slantBrightness = LedConstant.BRIGHTNESS_MAX
         set(value) {
             field = value
             binding.imgDeviceLedVline.alpha = translateBrightnessToAlpha(value)
         }
 
-    var centerBrightness = 4095
+    var centerBrightness = LedConstant.BRIGHTNESS_MAX
         set(value) {
             field = value
             binding.imgDeviceLedCenter.alpha = translateBrightnessToAlpha(value)
+            if (darkenCameraSection) {
+                binding.imgDeviceBackgroundOverlay.alpha = (value / LedConstant.BRIGHTNESS_MAX.toFloat()) * 0.1f
+            }
         }
 
-    var bottomStripBrightness = 4095
+    var bottomStripBrightness = LedConstant.BRIGHTNESS_MAX
         set(value) {
             field = value
             binding.imgDeviceLedBottom.alpha = translateBrightnessToAlpha(value)
         }
 
-    var bottomDotBrightness = 4095
+    var bottomDotBrightness = LedConstant.BRIGHTNESS_MAX
         set(value) {
             field = value
             binding.imgDeviceLedBottomDot.alpha = translateBrightnessToAlpha(value)
+        }
+
+    var allLedBrightness = 0
+        set(value) {
+            cameraBrightness = value
+            slantBrightness = value
+            centerBrightness = value
+            bottomStripBrightness = value
+            bottomDotBrightness = value
+            field = value
+        }
+
+    private val ledBottomStripIntensityDrawables = arrayOf(null, R.drawable.led_strip_bottom_1, R.drawable.led_strip_bottom_2, R.drawable.led_strip_bottom_3, R.drawable.led_strip_bottom_4, R.drawable.led_strip_bottom_5, R.drawable.led_strip_bottom_6, R.drawable.led_strip_bottom_7, R.drawable.led_strip_bottom_8)
+    var bottomStripIntensity = 8
+        set(value) {
+            if (value == 0) {
+                binding.imgDeviceLedBottom.setImageDrawable(null)
+            }
+            else {
+                binding.imgDeviceLedBottom.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        context,
+                        ledBottomStripIntensityDrawables[value]!!
+                    )
+                )
+            }
+            field = value
         }
 
     fun setOnCameraSectionClickListener(listener: OnClickListener) = binding.viewTouchLedCamera.setOnClickListener(listener)
