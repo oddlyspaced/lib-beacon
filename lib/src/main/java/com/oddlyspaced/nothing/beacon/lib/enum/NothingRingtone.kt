aae7aabd3bb7f76bc7775c0e30e6100971e4a75c
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-07 18:04:47 +0530

    lib: NothingRingtone: add name field to enums

--- lib/src/main/java/com/oddlyspaced/nothing/beacon/lib/enum/NothingRingtone.kt
+++ lib/src/main/java/com/oddlyspaced/nothing/beacon/lib/enum/NothingRingtone.kt
@@ -2,15 +2,15 @@ package com.oddlyspaced.nothing.beacon.lib.enum
 
 import com.oddlyspaced.nothing.beacon.lib.R
 
-enum class NothingRingtone(val media: Int, val data: Int) {
-    ABRA(R.raw.ringtone_abra, R.raw.data_abra),
-    BEETLE(R.raw.ringtone_beetle, R.raw.data_beetle),
-    CODED(R.raw.ringtone_coded, R.raw.data_coded),
-    PLOT(R.raw.ringtone_plot, R.raw.data_plot),
-    PNEUMATIC(R.raw.ringtone_pneumatic, R.raw.data_pneumatic),
-    RADIATE(R.raw.ringtone_radiate, R.raw.data_radiate),
-    SCRIBBLE(R.raw.ringtone_scribble, R.raw.data_scribble),
-    SNAPS(R.raw.ringtone_snaps, R.raw.data_snaps),
-    SQUIRRELS(R.raw.ringtone_squirrels, R.raw.data_squirrels),
-    TENNIS(R.raw.ringtone_tennis, R.raw.data_tennis),
+enum class NothingRingtone(val media: Int, val data: Int, name: String) {
+    ABRA(R.raw.ringtone_abra, R.raw.data_abra, "abra"),
+    BEETLE(R.raw.ringtone_beetle, R.raw.data_beetle, "beetle"),
+    CODED(R.raw.ringtone_coded, R.raw.data_coded, "coded"),
+    PLOT(R.raw.ringtone_plot, R.raw.data_plot, "plot"),
+    PNEUMATIC(R.raw.ringtone_pneumatic, R.raw.data_pneumatic, "pneumatic"),
+    RADIATE(R.raw.ringtone_radiate, R.raw.data_radiate, "radiate"),
+    SCRIBBLE(R.raw.ringtone_scribble, R.raw.data_scribble, "scribble"),
+    SNAPS(R.raw.ringtone_snaps, R.raw.data_snaps, "snaps"),
+    SQUIRRELS(R.raw.ringtone_squirrels, R.raw.data_squirrels, "squirrels"),
+    TENNIS(R.raw.ringtone_tennis, R.raw.data_tennis, "tennis"),
 }
